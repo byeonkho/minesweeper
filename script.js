705cef47c22b49413bbaf117a71323bf1c58807c
byeonkho <byeon.kho@gmail.com>
2023-03-31 11:55:09 +0800

    updated grid generator function
    added isRevealed, isMine flags to obj creation

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const gridContainer = [];
 const generateGrid = (gridLength) => {
   for (let x = 1; x <= gridLength; x++) {
     for (let y = 1; y <= gridLength; y++) {
-      const newCellObj = { coords: [x, y] };
+      const newCellObj = { coords: [x, y], isRevealed: false, isMine: false};
       gridContainer.push(newCellObj);
     }
   }
